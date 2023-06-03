ef6e1c027a6b279c67c12e535b5dc538bc816c88
Matthew Lang Sottile <me@matthewLS.com>
2023-06-03 15:12:13 -0400

    add cart total calc, modal checkout

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -6,6 +6,9 @@ export default function CartContextProvider(props){
     //create global state to hold cart
     const [cart, setCart] = useState([])
 
+    //create global state to hold cart total price
+    const [cartTotalPrice, setCartTotalPrice] = useState(0)
+
     useEffect(
         ()=>{
             console.log('context loaded')
@@ -16,7 +19,7 @@ export default function CartContextProvider(props){
             //if so use that value to initialize
             if (storedCart){
                 //set with this value
-                setCart(JSON.parse(storedCart))
+                setCart(JSON.parse(storedCart))       
             }
 
         }, [] //run one time when contex loads
@@ -27,15 +30,30 @@ export default function CartContextProvider(props){
             //update locale storage every time the cart changes
             //save new state of cart when it changes
             localStorage.setItem('cartList', JSON.stringify(cart))
+        
+            console.log('cartTotalPrice is $',cartTotalPrice)
         }, [cart]
     )
+    
 
+    // // // function to calulate cart total price
+    // const calcCartTotal = (newCart) => {
+    //     let newTotal = 
+    //     newCart.reduce((total, item) => total.price + item.price,0)
+    //     // console.log(result)
+    //     setCartTotalPrice(newTotal.toFixed(2))
+    // }
+   
     // function will Add Product to Cart
     const addProductToCart = (productToAdd) => {
         console.log('adding', productToAdd)
         let newCart = [...cart, productToAdd]
         setCart(newCart)
-        console.log(cart)
+        //
+        console.log('new cart is', newCart)
+        let newTotal = newCart.reduce((total, item) => total + item.price,0)
+        console.log('total is',newTotal)
+        setCartTotalPrice(newTotal.toFixed(2))
     }
 
     //function to remove Producst from cart
@@ -44,10 +62,19 @@ export default function CartContextProvider(props){
         //filter will keep all that don't match product id 
         let newCart = cart.filter(item=>item.id !== productId)
         setCart(newCart)
+        //
+        console.log('new cart is', newCart)
+        let newTotal = newCart.reduce((total, item) => total + item.price,0,)
+        console.log('total is',newTotal)
+        setCartTotalPrice(newTotal.toFixed(2))
+    }
+    // function to clear cart
+    const clearCart = () => {
+        setCart([]);
     }
 
     return(
-        <CartContext.Provider value={{cart, addProductToCart, removeProductFromCart}}>
+        <CartContext.Provider value={{cart, cartTotalPrice, clearCart, addProductToCart, removeProductFromCart}}>
             {props.children}
         </CartContext.Provider>
     )
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,16 +1,55 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import "./Cart.css"
 import { CartContext } from '../../contexts/CartContext'
 import ProductCard from '../../components/ProductCard/ProductCard'
+import Modal from 'react-modal'
+import {Link} from 'react-router-dom'
+
 
 
 
 function Cart() {
+
+  //create state for modal
+const [isOpen, setIsOpen] = useState(false)
+
+  //create styling for modal
+  const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: '600px',
+      // backgroundColor: "green",
+      },
+    overlay:{
+      backgroundColor: "rgba(0,0,0,0.5)",
+    }
+  
+  }
+  
+  //bind modal to app element
+  Modal.setAppElement(document.getElementById("root"));
+  
     //show what is in the cart
     //they are in the cart context 
       //access global context for cart
   //uses {} not []
- const {cart} = useContext(CartContext)
+ const {cart, cartTotalPrice, clearCart} = useContext(CartContext)
+
+    
+  //function for closing the modal procedure
+  //clear cart then setIsOpenFalse
+  const closeModalProc = () => {
+    // set cart to empty array with function in cart context
+    clearCart();
+    // use setIsOpen method to close modal
+    setIsOpen(false);   
+  }
+
   return (
     <div className="cart-page-container">
         <div className="cart-headers">
@@ -26,8 +65,37 @@ function Cart() {
             :
             <p>No items in cart</p>
             }
-        </div>
-    </div>
+            </div>
+            {
+            (cart.length > 0) &&
+            <div>
+            <p>Total: ${cartTotalPrice}</p>
+            <button onClick={()=>setIsOpen(true)}>Checkout</button>
+            </div>
+            }
+            
+            
+              <Modal
+              isOpen={isOpen}
+              // onRequesetClose={()=>{closeModalProc()}}
+              onRequesetClose={()=>{setIsOpen(false)}}
+              shouldCloseOnOverlayClick={true}
+              style={modalStyles}
+              contentLabel = "Checkout Modal"
+              >
+              <div className="checkout-modal-container">
+
+                <p>Your Order was successful!</p>
+                <p>Check your email for the order confirmation. Thank you for shopping with Fake Store!</p>
+                <Link to="/"><button onClick={()=>{closeModalProc()}}>Return to Main Page</button></Link>
+                </div>
+              </Modal>
+              </div>
+           
+            
+            
+        
+    
   )
 }
 
